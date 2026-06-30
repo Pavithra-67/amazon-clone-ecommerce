043a10c13c3d076c44a5722a670b00aa509c4c98
Pavithra Sankari <pavi.sankari26@gmail.com>
2026-07-01 00:15:22 +0530

    Add connection timeout options and error handling for MongoDB

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URL).then ((con) => {
-        console.log('MongoDB connected to host: ' +con.connection.host)
+    mongoose.connect(process.env.DB_URL, {
+        serverSelectionTimeoutMS: 30000,
+        socketTimeoutMS: 45000,
     })
+    .then((con) => {
+        console.log('MongoDB connected to host: ' + con.connection.host)
+    })
+    .catch((err) => {
+        console.log('MongoDB connection error: ' + err.message)
+    });
 };
 
 module.exports = connectDatabase;
